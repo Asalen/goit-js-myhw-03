6e5baae548d214b5df6f071a871a22085adc6b8a
ANDRII ALENIN <mkalenin@gmail.com>
2021-01-23 11:59:40 +0200

    Удаляет старые задачи

--- js/task-07.js
+++
@@ -1,6 +0,0 @@
-// Стартовый код
-const fruits = ['яблоко', 'персик', 'груша', 'банан'];
-// Пиши код ниже этой строки
-const fruitsArrayLength = fruits.length;
-
-console.log(fruitsArrayLength);
--- js/task-08.js
+++ js/task-08.js
@@ -1,8 +1,1 @@
-// Стартовый код
-const fruits = ['яблоко', 'персик', 'груша', 'банан'];
-// Пиши код ниже этой строки
-const lastElementIndex = fruits.length - 1;
-const lastElement = fruits[lastElementIndex];
-
-console.log(lastElementIndex);
-console.log(lastElement);
+/
